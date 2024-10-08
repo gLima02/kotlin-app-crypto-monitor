79c7a1dd4efd947206d0c340dd5d14f1c459e864
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 21:59:10 -0300

    first commit / Ajustando o arquivo AndroidManifest.xml para aceitar a dependência appcompact

---
+++ app/src/main/java/glima02/com/github/kotlin_app_crypto_monitor/MainActivity.kt
@@ -0,0 +1,20 @@
+package glima02.com.github.kotlin_app_crypto_monitor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import glima02.com.github.kotlin_app_crypto_monitor.ui.theme.KotlinappcryptomonitorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
