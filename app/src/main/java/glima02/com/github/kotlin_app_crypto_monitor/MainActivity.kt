bb0525497b2491d3574d521516308ceea3c611ed
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 22:09:21 -0300

    ADD:dependecia retrofit para comunicacao entre APIs

--- app/src/main/java/glima02/com/github/kotlin_app_crypto_monitor/MainActivity.kt
+++ app/src/main/java/glima02/com/github/kotlin_app_crypto_monitor/MainActivity.kt
@@ -11,10 +11,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import glima02.com.github.kotlin_app_crypto_monitor.ui.theme.KotlinappcryptomonitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
