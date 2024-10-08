8f1ee2583569c03cb4375eca7f076d44616cae28
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 22:12:58 -0300

    finalizado, commit Criando o payload de resposta da API aberta do BITCOIN

---
+++ app/src/main/java/glima02/com/github/kotlin_app_crypto_monitor/model/TicketResponse.kt
@@ -0,0 +1,15 @@
+package glima02.com.github.kotlin_app_crypto_monitor.model
+
+class TickerResponse(
+    val ticker: Ticker
+)
+
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
